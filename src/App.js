b7788ab3b0b51a0970a97b0ab93fcd55cc964617
Oleksandr Kapetii <oleksandrkapetii@outlook.com>
2024-01-02 20:40:50 +0200

    Updated version
    -Deleted aren't needed files.
    -Add some functionality.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ export default App;
 
 function AddTaskForm({ handler }) {
   return (
-    <div className="AddTaskForm">
-      <p>Додати справу:</p>
+    <div className="add-task-form">
       <input
         title="new task"
         type="text"
@@ -12,6 +11,7 @@ function AddTaskForm({ handler }) {
         onKeyDown={(e) => {
           if (e.key === "Enter") handler();
         }}
+        placeholder="Додати нову справу"
       />
       <button id="addBtn" type="submit" onClick={handler}>
         Додати
@@ -20,15 +20,32 @@ function AddTaskForm({ handler }) {
   );
 }
 
+function Task({ task }) {
+  const [focus, setFocus] = useState(false);
+  if(focus) {
+  return (
+    <div className="task" onMouseMove={(e) => setFocus(true)} onMouseLeave={(e) => setFocus(false)}>
+      <input type="checkbox"></input>
+      <p className="task-text">{task.text}</p>
+      <p className="task-date">{task.date}</p>
+      <button>X</button>
+    </div>
+  )
+       } else {
+  return (
+    <div key={task.id} className="task" onMouseMove={(e) => setFocus(true)}>
+      <p className="task-text">{task.text}</p>
+      <p className="task-date">{task.date}</p>
+    </div>
+  );
+}
+}
+
 function TasksList({ listOfTasks }) {
   let tasksList = [];
-  console.log(tasksList);
   listOfTasks.map((task) => {
-    console.log(task);
     return tasksList.push(
-      <div className="task" key={listOfTasks.indexOf(task)}>
-        {task}
-      </div>
+      <Task key={task.id} id={listOfTasks.indexOf(task)} task={task}></Task>
     );
   });
   return tasksList;
@@ -38,11 +55,18 @@ function App() {
   const [tasks, setTasks] = useState([]);
   function addTask() {
     const input = document.getElementById("new-task");
-    const newTask = input.value;
+    const newTask = {
+      id: tasks.length + 1,
+      text: input.value,
+      isDone: false,
+      date: new Date().toLocaleDateString(),
+    };
     input.value = "";
+
     setTasks(tasks.concat(newTask));
-    tasks.push(newTask);
   }
+  console.log(tasks);
+
   return (
     <div className="App">
       <h1>Мій список справ</h1>
