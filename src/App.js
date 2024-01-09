97707296d8ad548fe47058a3ec0399ce6b35cdd2
Oleksandr Kapetii <oleksandrkapetii@outlook.com>
2024-01-09 16:33:42 +0200

    Last changes

--- src/App.js
+++ src/App.js
@@ -23,40 +23,50 @@ function AddTaskForm({ handler }) {
 function Task({ task, handlerChangeIsDone, handlerTaskDelete }) {
   const [isDone, setIsDone] = useState(task.isDone);
 
-    return (
-      <div
-        className={"task" + (isDone? " task-isDone" : "")}
-      >
-        <input
+  return (
+    <div className={"task" + (isDone ? " task-isDone" : "")}>
+      <input
         className="task-checkbox"
-          type="checkbox"
-          onChange={(e) => {
-            handlerChangeIsDone(task.id);
-            setIsDone(!isDone);
-          }}
-          checked={isDone}
-        />
-        <p className="task-text">{isDone ? <s>{task.text}</s> : task.text}</p>
-        <p className="task-date">{task.date}</p>
-        <button className="task-delete-button" onClick={(e) => handlerTaskDelete(task.id)}>X</button>
-      </div>
-    );
+        type="checkbox"
+        onChange={(e) => {
+          handlerChangeIsDone(task.id);
+          setIsDone(!isDone);
+        }}
+        checked={isDone}
+      />
+      <p className="task-text">{isDone ? <s>{task.text}</s> : task.text}</p>
+      <p className="task-date">{task.date}</p>
+      <button
+        className="task-delete-button"
+        onClick={(e) => handlerTaskDelete(task.id)}
+      >
+        X
+      </button>
+    </div>
+  );
 }
 
 function TasksList({ tasks, handlerChangeIsDone, handlerTaskDelete }) {
   let tasksList = [];
-  tasks.map((task) => {
-    return tasksList.push(
-      <Task
-        key={task.id}
-        id={tasks.indexOf(task)}
-        task={task}
-        tasks={tasks}
-        handlerChangeIsDone={handlerChangeIsDone}
-        taskDelete={handlerTaskDelete}
-      ></Task>
-    );
-  });
+  tasks
+    .sort((a, b) => {
+      if (a.isDone === b.isDone) {
+        return 0;
+      }
+      return a.isDone ? 1 : -1;
+    })
+    .map((task) => {
+      return tasksList.push(
+        <Task
+          key={task.id}
+          id={tasks.indexOf(task)}
+          task={task}
+          tasks={tasks}
+          handlerChangeIsDone={handlerChangeIsDone}
+          handlerTaskDelete={handlerTaskDelete}
+        ></Task>
+      );
+    });
   return <div className="tasks-list">{tasksList}</div>;
 }
 
@@ -65,15 +75,17 @@ function App() {
 
   function handlerAddTask() {
     const input = document.getElementById("new-task");
-    const newTask = {
-      id: tasks.length + 1,
-      text: input.value,
-      isDone: false,
-      date: new Date().toLocaleDateString(),
-    };
-    input.value = "";
+    if (input.value && input.value.length > 1) {
+      const newTask = {
+        id: tasks.length + 1,
+        text: input.value,
+        isDone: false,
+        date: new Date().toLocaleDateString(),
+      };
+      input.value = "";
 
-    setTasks(tasks.concat(newTask));
+      setTasks(tasks.concat(newTask));
+    }
   }
 
   function handlerChangeIsDone(id) {
@@ -87,14 +99,18 @@ function App() {
   }
 
   function handlerTaskDelete(id) {
-    const newTasks = tasks.filter((task) => task.id!== id);
+    const newTasks = tasks.filter((task) => task.id !== id);
     setTasks(newTasks);
   }
 
   return (
     <div className="App">
       <AddTaskForm handler={handlerAddTask} />
-      <TasksList tasks={tasks} handlerChangeIsDone={handlerChangeIsDone} handlerTaskDelete={handlerTaskDelete}></TasksList>
+      <TasksList
+        tasks={tasks}
+        handlerChangeIsDone={handlerChangeIsDone}
+        handlerTaskDelete={handlerTaskDelete}
+      ></TasksList>
     </div>
   );
 }
